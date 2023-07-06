2e1a720a96687617b5cab214f2547bd437168e88
Jonathan <jonathanle1111@gmail.com>
2023-07-05 22:41:21 -0500

    first commit

---
+++ MazeGeneration/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Use Krusall Algorithm to use a maze. 
+        // Genernal idea is image each spot as a node and create edges then check if they are valid. 
+        // Hardset part will be implement the krusall background information
+
+        System.out.println("Hello! Welcome to Maze Generation. We will use a maze of size 3x3 for testing");
+        
+    }
+}
---
+++ MazeGeneration/src/DFS.java
@@ -0,0 +1,8 @@
+/*
+ *  This file will hold the Depth-first search algorithm for maze generation.
+ * 
+ */
+
+public class DFS {
+    
+}
